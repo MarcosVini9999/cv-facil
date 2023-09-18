a338c58e837bcfc94b0ca0c24c42c7f26c032c39
Arthur Silva <73958393+ArthurMTS@users.noreply.github.com>
2023-09-18 08:15:28 -0300

    chore: user context

---
+++ src/config/interfaces.ts
@@ -0,0 +1,6 @@
+export interface UserProps {
+  avatar: string;
+  name: string;
+  email: string;
+  cvs: [];
+}
---
+++ src/contexts/user.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface UserProviderProps {
+  user: UserProps
+  setUser: (user: UserProps) => void;
+}
+
+import { UserProps } from "@/config/interfaces";
+
+export const UserContext = React.createContext({} as UserProviderProps);
+
+export function UserProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = React.useState<UserProps>({} as UserProps);
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
